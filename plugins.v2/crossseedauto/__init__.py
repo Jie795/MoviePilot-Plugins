9e34b28bfff29de864e5a64c258b1f7dd3b6552d
J <779076041@qq.com>
2026-02-15 22:56:04 +0800

    chore(crossseedauto): 更新插件版本号到1.1
    - 将插件内部版本号从1.0更新为1.1
    - 修改package.v2.json中的版本号为1.1
    - 保持插件图标和作者信息不变

--- plugins.v2/crossseedauto/__init__.py
+++ plugins.v2/crossseedauto/__init__.py
@@ -29,7 +29,7 @@ class CrossSeedAuto(_PluginBase):
     # 插件图标
     plugin_icon = "crossseed.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "zhjay"
     # 作者主页
